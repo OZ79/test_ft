36c4506ba90f3f14ddf87fe483f40e83a2bad69f
Oleksandr Zazhytskyi <alexazaz@meta.ua>
2025-04-15 19:00:15 +0300

    Correction .gitignore

---
+++ lib/generated/assets/assets.gen.dart
@@ -0,0 +1,30 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/moon_icon.svg
+  String get moonIcon => 'assets/icons/moon_icon.svg';
+
+  /// File path: assets/icons/search_icon.svg
+  String get searchIcon => 'assets/icons/search_icon.svg';
+
+  /// File path: assets/icons/sun_icon.svg
+  String get sunIcon => 'assets/icons/sun_icon.svg';
+
+  /// List of all assets
+  List<String> get values => [moonIcon, searchIcon, sunIcon];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+}
